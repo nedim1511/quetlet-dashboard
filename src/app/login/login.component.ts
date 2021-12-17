da0d8caabb7e8f6edaeee227ede4499407936e10
Nedim <kurbegovicnedim@gmail.com>
2021-12-17 19:08:26 +0100

    add form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,4 +5,19 @@ import {Component} from "@angular/core";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public code: string | undefined;
+  public emailAddress: string | undefined;
+
+  public wrongLoginError: boolean;
+  public loginButtonDisabled: boolean;
+
+  constructor() {
+    this.wrongLoginError = false;
+    this.loginButtonDisabled = false;
+  }
+
+  loginButtonClicked(): void {
+    this.loginButtonDisabled = true;
+  }
+}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from "@angular/core";
 import {LoginComponent} from "./login.component";
 import {LoginRoutingModule} from "./login-routing.module";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
+    FormsModule,
+    CommonModule,
     LoginRoutingModule,
   ],
   declarations: [
