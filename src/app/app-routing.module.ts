1b37dc334972ef7813879e6afca92a0600c2d942
Nedim <kurbegovicnedim@gmail.com>
2021-12-17 18:48:42 +0100

    create login page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
